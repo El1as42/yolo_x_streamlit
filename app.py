abb25481a2599104daf95be2b6ee9b89f629677f
Elias <schlosser.elias@web.de>
2024-10-15 19:54:01 +0200

    upgrade to large model

--- app.py
+++ app.py
@@ -3,25 +3,23 @@ from ultralytics import YOLO
 import numpy as np
 from PIL import Image
 
-# Load the YOLOv8 model (adjust to the appropriate path if needed)
-model = YOLO("yolov8n.pt")  # Ensure you have the right path and model
 
-# Streamlit app interface
-st.title("YOLOv8 Object Detection")
+model = YOLO("yolo11l.pt")  # This single parameter determines which yolo version and size is used
 
+st.title("YOLO11l Object Detection")
 enable = st.checkbox("Enable camera")
 picture = st.camera_input("Take a picture", disabled=not enable)
 
 if picture:
     # Convert the picture from the camera input to a format usable by OpenCV
     image = Image.open(picture)
-    image = np.array(image)  # Convert to NumPy array (OpenCV format: BGR)
+    # Convert to NumPy array (OpenCV format: BGR)
+    image = np.array(image)  
 
-    # Run YOLOv8 inference on the image
-    st.write("Running YOLOv8 object detection...")
-
+    st.write("Running YOLO11l object detection...")
     try:
-        results = model(image)  # Perform inference on the input image
+        # Perform inference on the input image
+        results = model(image)  
 
         # Check if YOLO detected any objects
         if results and len(results[0].boxes) > 0:
@@ -30,9 +28,9 @@ if picture:
             st.write("No objects detected.")
 
         # Annotate the image with bounding boxes and labels
-        annotated_frame = results[0].plot()  # YOLOv8 automatically annotates the image
+        annotated_frame = results[0].plot()
 
         # Display the annotated image in Streamlit
-        st.image(annotated_frame, caption="Detected objects with YOLOv8", use_column_width=True)
+        st.image(annotated_frame, use_column_width=True)
     except Exception as e:
-        st.error(f"Error during YOLOv8 detection: {e}")
+        st.error(f"Error during object detection: {e}")
